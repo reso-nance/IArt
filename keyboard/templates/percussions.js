77822df5e923787ecb5911a8503d279bd257722b
random-dude <aezurivideo@gmail.com>
2019-09-03 10:37:40 -0400

    KBD instruments layout are now stored on separate js files each layout can use or not the note duration AI will play for the equivalent of half the DB, limited to 10s added various percussions with layout, audio and pics tweaked the html to make the page more responsive

---
+++ keyboard/templates/percussions.js
@@ -0,0 +1,9 @@
+const noteDuration = false;
+var sounds = [{id:"taiko", HRname:"", wav:"taiko.wav", img:"taiko.png"},
+                      {id:"clave", HRname:"", wav:"clave.wav", img:"clave.png"},
+                      {id:"guiro", HRname:"", wav:"guiro.wav", img:"guiro.png"},
+                      {id:"splash", HRname:"", wav:"splash.wav", img:"splash.png"},
+                      {id:"djembé", HRname:"", wav:"djembe.wav", img:"djembé.png"},
+                      {id:"darbouka", HRname:"", wav:"darbouka.wav", img:"darbouka.png"},
+                      {id:"tabla", HRname:"", wav:"tabla.wav", img:"tabla.png"},
+                      {id:"cloche", HRname:"", wav:"cowbell.wav", img:"cowbell.png"}];
---
+++ keyboard/templates/test.js
@@ -0,0 +1,8 @@
+const noteDuration = false;
+var sounds = [{id:"canarie", HRname:"canarie", wav:"birds008.wav", img:"canary.png"},
+                      {id:"paon", HRname:"paon", wav:"birds009.wav", img:"peacock.jpg"},
+                      {id:"doigts", HRname:"doigts", wav:"Finger-Snaps.wav", img:"fingerSnaps.jpg"},
+                      //corrupted audio {id:"d", HRname:" ", wav:"bell.wav", img:"bell.jpg"},
+                      {id:"sax", HRname:"sax", wav:"barisax.wav", img:"barisax.jpg"},
+                      {id:"test", HRname:"test", wav:"barisax.wav"},
+                      {id:"kick", HRname:"kick", wav:"timpani.wav", img:"bassdrum.jpg"}];
